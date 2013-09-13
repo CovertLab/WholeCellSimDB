0d12a3d1b5eedeea76cdc32e68e2e17e7d8c1451
Jonathan Karr <jonrkarr@gmail.com>
2013-09-13 16:51:52 -0700

    moving web interface to separate app: wcdbweb

--- WholeCellDB/settings.py
+++ WholeCellDB/settings.py
@@ -127,6 +127,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'wcdb',
+	'wcdbweb',
 )
 
 # A sample logging configuration. The only tangible logging
