4721c13221bfb7e28b37b8c90bb34f0457afd79b
Jonathan Karr <jonrkarr@gmail.com>
2013-09-13 16:35:59 -0700

    removing deleted properties

--- wcdb/admin.py
+++ wcdb/admin.py
@@ -3,8 +3,8 @@ from wcdb.models import *
 
 class SimulationAdmin(admin.ModelAdmin):
     date_hierarchy = 'date'
-    list_display = ('name', 'model', 'organism', 't', 'date')
-    list_filter = ('date', 'model', 'organism')
+    list_display = ('name', 'organism', 'length', 'date')
+    list_filter = ('date', 'organism')
 
 
 admin.site.register(Simulation, SimulationAdmin)
