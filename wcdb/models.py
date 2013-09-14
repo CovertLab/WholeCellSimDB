7aad89dd221b0e632844946b043296e77e5eac65
Jonathan Karr <jonrkarr@gmail.com>
2013-09-13 17:03:53 -0700

    debugging importing HDF5_ROOT setting

--- wcdb/models.py
+++ wcdb/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 import h5py
 import sys
-from settings import HDF5_ROOT
+from WholeCellDB.settings import HDF5_ROOT
 
 ### OPP ###
 class Option(models.Model):
