e4a307167b8a0191861de43b760094f0fd9ae0ad
Jonathan Karr <jonrkarr@gmail.com>
2013-09-17 19:43:17 -0700

    modifying data model, adding pages

---
+++ wcdbweb/templatetags/template_extras.py
@@ -0,0 +1,41 @@
+from dateutil.tz import tzlocal
+from django import template
+from WholeCellDB import settings
+import datetime
+import math
+import os
+
+register = template.Library()
+
+@register.filter
+def columns(arr, n_cols):    
+    n_cols = int(float(n_cols))
+    
+    col_arr = []
+    idx = 0
+    for i in range(n_cols):
+        idx2 = int(math.ceil((i + 1.) * (len(arr) + 0.) / (n_cols + 0.)))
+        col_arr.append(arr[idx:idx2])
+        idx = idx2
+        
+    return col_arr
+
+@register.filter
+def order_by(qs, field):
+    return qs.order_by(field)
+    
+@register.filter
+def is_dict(obj):
+    return isinstance(obj, dict)
+    
+@register.filter
+def is_list(obj):
+    return isinstance(obj, list)
+    
+@register.filter
+def get_template_last_updated(templateFile):
+	return datetime.datetime.fromtimestamp(os.path.getmtime(settings.TEMPLATE_DIRS[0] + '/' + templateFile), tzlocal())
+    
+@register.filter
+def set_time_zone(datetime):
+	return datetime.replace(tzinfo=tzlocal())
