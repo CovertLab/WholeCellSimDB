111ca54d0e68c933fede4cb5cc311c0d09da6d76
Jonathan Karr <jonrkarr@gmail.com>
2014-03-05 21:45:24 -0800

    displaying units on batch page

--- wcdb/helpers.py
+++ wcdb/helpers.py
@@ -5,7 +5,7 @@ def get_option_dict(batch):
     options = OrderedDict()
 
     for opt in batch.options.filter(process=None, state=None).order_by('name').values('name').annotate(Count('name')):
-        tmp2 = [x[0] for x in batch.options.filter(process=None, state=None, name=opt['name']).order_by('index').values_list('value')] 
+        tmp2 = list(batch.options.filter(process=None, state=None, name=opt['name']).order_by('index').values('value', 'units'))
         if len(tmp2) == 1:
             tmp2 = tmp2[0]
         options[opt['name']] = tmp2
@@ -14,7 +14,7 @@ def get_option_dict(batch):
     for p in batch.processes.order_by('name'):
         tmp = OrderedDict()
         for opt in p.options.order_by('name').values('name').annotate(Count('name')):
-            tmp2 = [x[0] for x in p.options.filter(name=opt['name']).order_by('index').values_list('value')]
+            tmp2 =list(p.options.filter(name=opt['name']).order_by('index').values('value', 'units'))
             if len(tmp2) == 1:
                 tmp2 = tmp2[0]
             tmp[opt['name']] = tmp2
@@ -25,7 +25,7 @@ def get_option_dict(batch):
     for s in batch.states.order_by('name'):
         tmp = OrderedDict()
         for opt in s.options.order_by('name').values('name').annotate(Count('name')):
-            tmp2 = [x[0] for x in s.options.filter(name=opt['name']).order_by('index').values_list('value')]
+            tmp2 = list(s.options.filter(name=opt['name']).order_by('index').values('value', 'units'))
             if len(tmp2) == 1:
                 tmp2 = tmp2[0]
             tmp[opt['name']] = tmp2
@@ -39,7 +39,7 @@ def get_parameter_dict(batch):
     parameters = OrderedDict()
         
     for opt in batch.parameters.filter(process=None, state=None).order_by('name').values('name').annotate(Count('name')):
-        tmp2 = [x[0] for x in batch.parameters.filter(process=None, state=None, name=opt['name']).order_by('index').values_list('value')] 
+        tmp2 = list(batch.parameters.filter(process=None, state=None, name=opt['name']).order_by('index').values('value', 'units'))
         if len(tmp2) == 1:
             tmp2 = tmp2[0]
         parameters[opt['name']] = tmp2
@@ -48,7 +48,7 @@ def get_parameter_dict(batch):
     for p in batch.processes.order_by('name'):
         tmp = OrderedDict()
         for opt in p.parameters.order_by('name').values('name').annotate(Count('name')):
-            tmp2 = [x[0] for x in p.parameters.filter(name=opt['name']).order_by('index').values_list('value')]
+            tmp2 = list(p.parameters.filter(name=opt['name']).order_by('index').values('value', 'units'))
             if len(tmp2) == 1:
                 tmp2 = tmp2[0]
             tmp[opt['name']] = tmp2
@@ -59,7 +59,7 @@ def get_parameter_dict(batch):
     for s in batch.states.order_by('name'):
         tmp = OrderedDict()
         for opt in s.parameters.order_by('name').values('name').annotate(Count('name')):
-            tmp2 = [x[0] for x in s.parameters.filter(name=opt['name']).order_by('index').values_list('value')]
+            tmp2 = list(s.parameters.filter(name=opt['name']).order_by('index').values('value', 'units'))
             if len(tmp2) == 1:
                 tmp2 = tmp2[0]
             tmp[opt['name']] = tmp2
